c16003c9c886388899cf3f6ae9d52646b274b2f1
onouek <kohsuke.onoue@gmail.com>
2021-01-01 12:19:40 +0900

    Update setup script
    Signed-off-by: onouek <kohsuke.onoue@gmail.com>

--- server/scripts/server_setup.sh
+++ server/scripts/server_setup.sh
@@ -20,21 +20,15 @@ ufw allow 80
 ufw allow 443
 ufw enable
 
+# Web app file structure
+mkdir /apps
+chmod 777 /apps
+mkdir -p /apps/logs/weather_api/app_log
 
 apt install acl -y
 useradd -M apiuser
 usermod -L apiuser
 setfacl -m u:apiuser:rwx /apps/logs/weather_api
-
-
-# Web app file structure
-mkdir /apps
-chmod 777 /apps
-mkdir /apps/logs
-mkdir /apps/logs/weather_api
-mkdir /apps/logs/weather_api/app_log
-# chmod 777 /apps/logs/weather_api
-cd /apps
 
 # Create a virtual env for the app.
 cd /apps
@@ -46,14 +40,14 @@ pip install --upgrade gunicorn uvloop httptools
 
 # clone the repo:
 cd /apps
-git clone https://github.com/talkpython/modern-apis-with-fastapi app_repo
+git clone https://github.com/onouek/weather app_repo
 
 # Setup the web app:
-cd /apps/app_repo/ch08-deployment
+cd /apps/app_repo/
 pip install -r requirements.txt
 
 # Copy and enable the daemon
-cp /apps/app_repo/ch08-deployment/server/units/weather.service /etc/systemd/system/
+cp /apps/app_repo/server/units/weather.service /etc/systemd/system/
 
 systemctl start weather
 systemctl status weather
@@ -65,7 +59,7 @@ apt install nginx
 # CAREFUL HERE. If you are using default, maybe skip this
 rm /etc/nginx/sites-enabled/default
 
-cp /apps/app_repo/ch08-deployment/server/nginx/weather.nginx /etc/nginx/sites-enabled/
+cp /apps/app_repo/server/nginx/weather.nginx /etc/nginx/sites-enabled/
 update-rc.d nginx enable
 service nginx restart
 
@@ -73,6 +67,6 @@ service nginx restart
 # Optionally add SSL support via Let's Encrypt:
 # https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-18-04
 
-add-apt-repository ppa:certbot/certbot
-apt install python3-certbot-nginx
-certbot --nginx -d weatherapi.talkpython.com
+#add-apt-repository ppa:certbot/certbot
+#apt install python3-certbot-nginx
+#certbot --nginx -d weatherapi.talkpython.com
